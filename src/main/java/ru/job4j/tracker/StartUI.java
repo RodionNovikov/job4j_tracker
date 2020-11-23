be60959ea814591180c3668e2631ac1d71db83c5
Rodion <novicovrv@gmail.com>
2020-11-23 15:42:54 +0300

    7. toString [#375237]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,15 +1,9 @@
 package ru.job4j.tracker;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 public class StartUI {
 
     public static void main(String[] args) {
-        Item time = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        LocalDateTime localTime = time.getLocalDateTime();
-        String currentDateTime = localTime.format(formatter);
-        System.out.println("Текущие дата и время после форматирования: " + currentDateTime);
+        Item user = new Item(1, "Rodion");
+        System.out.println(user);
 
     }
 }
